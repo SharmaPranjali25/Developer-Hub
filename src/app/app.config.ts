055deebfefe71afc72f65971c2f98f633d8a8297
Pranjali Sharma <sharmapranjali115@gmail.com>
2026-03-23 18:32:16 +0530

    Auth0service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient} from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { withInterceptors } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { authHttpInterceptorFn } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
+    provideAuth0({
+      domain: environment.auth.domain,
+      clientId: environment.auth.clientId,
+      authorizationParams:{
+        redirect_uri: environment.auth.redirectUri
+      }
+    }),
+
   ]
 };
